05844276efbf91e30179fcc889b1875ad41bda02
nabsskrrtt <nabilaputrii2035@gmail.com>
2024-10-09 04:57:05 +0700

    Tugas 6 - README

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,20 @@
 from django.forms import ModelForm
 from main.models import Bunga
+from django.utils.html import strip_tags
 
 class BungaForm (ModelForm):
     class Meta:
         model = Bunga
         fields = ["name", "price", "description", "stocks", "img_url"]
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        return strip_tags(name)
+
+    def clean_description(self):
+        description = self.cleaned_data["description"]
+        return strip_tags(description)
+
+    def clean_img_url(self):
+        img_url = self.cleaned_data["img_url"]
+        return strip_tags(img_url)
--- main/urls.py
+++ main/urls.py
@@ -10,6 +10,7 @@ from main.views import login_user
 from main.views import logout_user
 from main.views import edit_bunga
 from main.views import delete_bunga
+from main.views import add_flower_entry_ajax
 
 app_name = 'main'
 
@@ -25,4 +26,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-bunga/<uuid:id>', edit_bunga, name='edit_bunga'),
     path('delete/<uuid:id>', delete_bunga, name='delete_bunga'),
+    path('create-flower-entry-ajax', add_flower_entry_ajax, name='add_flower_entry_ajax')
 ]
